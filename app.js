243cbfbe443b968e2515d2d6a64e8bf41b4af83d
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-07 00:39:56 +0530

    javascript updated

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#check-number");
+var outputBox = document.querySelector("#output-box");
 
-
-function compareValues(sum,luckyNumber){
-    if (sum%luckyNumber===0){
-        console.log("Your birthday is lucky🤩")
-    }else{
-        console.log("Your birthday is not lucky😓")
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText="Your birthday is lucky🤩"
+    } else {
+        outputBox.innerText="Your birthday is not lucky😓"
     }
 }
-function checkBirthdayIsLucky(){
-    var dob=dateOfBirth.value ;
-    var sum= calculateSum(dob);
-    compareValues(sum,luckyNumber.value)
+
+function checkBirthdayIsLucky() {
+    var dob = dateOfBirth.value;
+    var sum = calculateSum(dob);
+    
+    if (sum && dob) {
+        compareValues(sum, luckyNumber.value)
+    } else {
+        outputBox.innerText = "Please enter both the fields🥴";
+    }
+
 }
-function calculateSum(dob){
-    dob = dob.replaceAll("-","");
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
     let sum = 0;
-    for (let index=0; index<dob.length; index++){
+    for (let index = 0; index < dob.length; index++) {
         sum = sum + Number(dob.charAt(index));
     }
     return sum;
 }
 
 
+
 checkButton.addEventListener("click", checkBirthdayIsLucky);
-
-
-
-
-
